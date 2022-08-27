56e28c15dc573a625e4a155308b60a9f3189b52f
SacredDevKing <sacreddevking@gmail.com>
2022-08-27 10:07:08 -0700

    Small tweak to users resolver

--- schema/resolvers.js
+++ schema/resolvers.js
@@ -7,9 +7,9 @@
  * It can populate that data in any way you define, such as by fetching data from a back-end
  * database or a third-party API.
  */
-const UsersList = require("./sampleData");
-const MoviesList = require("./MoviesList");
-const _ = require("lodash");
+const UsersList = require('./sampleData');
+const MoviesList = require('./MoviesList');
+const _ = require('lodash');
 
 const resolvers = {
   Query: {
@@ -19,7 +19,6 @@ const resolvers = {
     },
     user: (parent, args) => {
       // Logic to return specific user by id
-      console.log(parent);
       const id = args.id;
       const user = _.find(UsersList, { id: Number(id) });
 
