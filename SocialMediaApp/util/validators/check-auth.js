b1f5ae2f188309c720e740519d713493582ae1fd
SacredDevKing <sacreddevking@gmail.com>
2022-09-14 07:58:31 -0700

    Finish created auth check file to compare jwt tokens

--- SocialMediaApp/util/validators/check-auth.js
+++ SocialMediaApp/util/validators/check-auth.js
@@ -1,3 +1,25 @@
 const jwt = require('jsonwebtoken');
+const { AuthenticationError } = require('apollo-server');
+module.exports = (context) => {
+  const authHeader = context.req.headers.authorization;
+  // Get token
+  const token = authHeader.split('Bearer ')[1];
 
-module.exports = (context) => {};
+  if (!authHeader) {
+    // Bail out early here
+    throw new Error('Authentication header is missing');
+  }
+
+  if (!token) {
+    throw new Error('Auth token must be formatted as: Bearer [token]');
+  }
+
+  // If all is well then verify the users token
+  try {
+    // Verify user
+    return jwt.verify(token, process.env.JWT_SECRET_KEY);
+  } catch (error) {
+    console.error(error);
+    throw new AuthenticationError('Invalid/Expired token');
+  }
+};
