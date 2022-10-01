efc496bbaa685828de61ea87a1e6b3a90fc6bb14
SacredDevKing <sacreddevking@gmail.com>
2022-10-01 13:58:25 -0700

    Initial commit for client react app

---
+++ SocialMediaApp/client/src/App.js
@@ -0,0 +1,13 @@
+import './App.css';
+
+function App() {
+  return (
+    <div className='App'>
+      <header className='App-header'>
+        <h1>Initial App Comp.</h1>
+      </header>
+    </div>
+  );
+}
+
+export default App;
